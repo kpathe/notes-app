4662ddac38e43b53878d6d258f9f6ebcef216152
Kaushlendra Pathe <kpathe972@gmail.com>
2026-09-18 08:31:23 +0530

    fix: ensure async handling in note actions and improve note retrieval logic

--- app/actions/notes.ts
+++ app/actions/notes.ts
@@ -7,15 +7,15 @@ import { addNote, toggleImportance } from "../services/notes";
 export const createNote = async (formData: FormData) => {
   const content = formData.get("content") as string;
   const important = formData.get("important") === "on";
-  addNote(content, important);
+  await addNote(content, important);
 
   revalidatePath("/notes");
   redirect("/notes");
 };
 
-export const toggleNoteImportance = async (formData: FormData) =>{
-  const id = Number(formData.get("id"))
-  toggleImportance(id)
+export const toggleNoteImportance = async (formData: FormData) => {
+  const id = Number(formData.get("id"));
+  await toggleImportance(id);
   revalidatePath(`/notes/${id}`);
-  revalidatePath("/notes")
-}
+  revalidatePath("/notes");
+};
